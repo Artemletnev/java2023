d5607b7c4888b4b1e8ef024b7b3228e688cfcf32
Артём <letnev2012@gmail.com>
2024-01-26 19:08:41 +0300

    lesson3

---
+++ src/main/java/Lesson1_3/TwoDimArrayApp.java
@@ -0,0 +1,52 @@
+package Lesson1_3;
+
+import java.util.Arrays;
+
+public class TwoDimArrayApp {
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
+        System.out.println(Arrays.toString(array));
+
+        array[2][3] = 144;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(Arrays.toString(array[i]));
+
+        }
+        //System.out.println();
+
+        int[] subArray = array[2];
+        System.out.println(Arrays.toString(subArray));
+        System.out.println();
+
+        int[][] newArray = new int[5][];
+        newArray[0] = new int[]{1, 2, 3, 4, 5, 6};
+        newArray[3] = new int[]{2, 3, 4, 5, 1};
+
+        System.out.println(Arrays.toString(newArray));
+
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.println(Arrays.toString(newArray[i]));
+
+        }
+        System.out.println();
+        int[][] arr = {{1,2},{3,4}, {123,221, 1232}};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+
+        }
+        //int[][][][][] - многомерные массивы
+        char[] chars = "Java".toCharArray();
+
+        System.out.println(chars[2]);
+        System.out.println("Java".charAt(3));
+
+
+    }
+}
