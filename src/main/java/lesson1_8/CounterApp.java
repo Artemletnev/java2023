f9ab55811efdf797366c730ccd10223b936b4e3e
Артём <letnev2012@gmail.com>
2024-03-26 15:38:59 +0300

    lesson 8

---
+++ src/main/java/lesson1_8/CounterApp.java
@@ -0,0 +1,72 @@
+package lesson1_8;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CounterApp extends JFrame {
+
+    private int value;
+    private JLabel infoLable;
+
+
+    public CounterApp() {
+        setBounds(500, 500, 300, 120);
+        setTitle("Counter App");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        Font font = new Font("Arial", Font.BOLD, 32);
+
+        JLabel label = new JLabel(String.valueOf(value));
+        label.setFont(font);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
+
+        infoLable = new JLabel();
+        add(infoLable, BorderLayout.NORTH);
+
+        JButton decrementButton = new JButton("<");
+        decrementButton.setFont(font);
+        add(decrementButton, BorderLayout.WEST);
+
+        JButton incrementButton = new JButton(">");
+        incrementButton.setFont(font);
+        add(incrementButton, BorderLayout.EAST);
+
+        decrementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                value--;
+                label.setText(String.valueOf(value));
+                validateRange();
+
+            }
+        });
+
+        incrementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                value++;
+                label.setText(String.valueOf(value));
+                validateRange();
+            }
+
+        });
+
+
+        setVisible(true);
+    }
+    private void validateRange(){
+        if (Math.abs(value) > 10) {
+            infoLable.setText("Value out of range");
+        } else {
+            infoLable.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new CounterApp();
+    }
+
+}
